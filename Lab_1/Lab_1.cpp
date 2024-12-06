793749922aa7adaa4394384867f24286439d5a0a
Lomekun <79419915+lomekun@users.noreply.github.com>
2024-12-06 15:22:23 +0300

    Update Lab_1.cpp

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-int main(){
+int main() {
 	setlocale(LC_ALL, "RUS");
 	cout << "Задание 1 \n";
 	cout << "int: " << sizeof(int) << " байт \n"
@@ -16,11 +16,13 @@ int main(){
 		<< "long double: " << sizeof(long double) << " байт \n"
 		<< "char: " << sizeof(char) << " байт \n"
 		<< "bool: " << sizeof(bool) << " байт \n";
-	
+
 	{
 		cout << "Задание 2 \n";
+		cout << "Введите число: \n";
 		int value;
 		cin >> value;
+		int num = value;
 		unsigned int order = 32;
 		unsigned int mask = 1 << order - 1;
 		for (int i = 1; i <= order; i++)
@@ -37,6 +39,18 @@ int main(){
 			}
 		}
 		cout << endl;
+		{
+			cout << "ИДЗ-6  для integer\n";
+			int n;
+			cout << "Введите бит с которого нужно начать\n";
+			cin >> n;
+			for (int i = 1; n >= 0; --n)
+			{
+				num ^= i << n;
+			}
+			cout << num;
+		}
+		cout << endl;
 	}
 
 	{
@@ -45,14 +59,15 @@ int main(){
 			int value;
 			float num = 1028;
 		};
+		cout << "Введите число: \n";
 		cin >> num;
-		unsigned int order = sizeof(float)*8;
+		unsigned int order = sizeof(float) * 8;
 		unsigned int mask = 1 << order - 1;
 		for (int i = 1; i <= order; i++)
 		{
 			putchar(value & mask ? '1' : '0');
 			value <<= 1;
-			if (i  == 9)
+			if (i == 9)
 			{
 				putchar(' ');
 			}
@@ -62,6 +77,18 @@ int main(){
 			}
 		}
 		cout << endl;
+		{
+			cout << "ИДЗ-6 для float \n";
+			int n;
+			cout << "Введите бит с которого нужно начать\n";
+			cin >> n;
+			for (int i = 1; n >= 0; --n)
+			{
+				value ^= i << n;
+			}
+			cout << num;
+		}
+		cout << endl;
 	}
 	{
 		cout << "Задание 4 \n";
@@ -69,9 +96,10 @@ int main(){
 			long long int value;
 			double num = 1028;
 		};
+		cout << "Введите число: \n";
 		cin >> num;
 		unsigned int order = sizeof(double) * 8;
-		unsigned long long mask = 1 << order - 1;
+		unsigned long long mask = static_cast<unsigned long long>(1) << order - 1;
 		for (int i = 1; i <= order; i++)
 		{
 			putchar(value & mask ? '1' : '0');
@@ -86,19 +114,34 @@ int main(){
 			}
 		}
 		cout << endl;
+		{
+			cout << "ИДЗ-6 для double\n";
+			int n;
+			cout << "Введите бит с которого нужно начать\n";
+			cin >> n;
+			for (int i = 1; n >= 0; --n)
+			{
+				value ^= i << n;
+			}
+			cout << num;
+		}
+		cout << endl;
 	}
+	/*
 	{
 		cout << "ИДЗ-6 \n";
-		int num, n;
+		int num;
+		int n;
 		cout << "Введите число: \n";
 		cin >> num;
 		cout << "Введите бит с которого нужно начать\n";
 		cin >> n;
-		for (int i = 1; n >= 0; --n) 
+		for (int i = 1; n >= 0; --n)
 		{
 			num ^= i << n;
 		}
 		cout << num;
 	}
+	*/
 	return 0;
 }
